37560cd293e0ef166dcb3cc9519e1c2c5e858e04
Sławomir Grzegorzewski <slawek.grz@gmail.com>
2019-07-31 23:51:04 +0200

    Initial commit

---
+++ src/main/java/pl/sg/accountant/AccountantApplication.java
@@ -0,0 +1,13 @@
+package pl.sg.accountant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountantApplication.class, args);
+    }
+
+}
