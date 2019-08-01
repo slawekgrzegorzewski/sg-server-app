70b52b423f15508f22d95b9dd88371ef9061d23e
Slawomir Grzegorzewski <slawomir.grzegorzewski@motorolasolutions.com>
2019-08-01 20:15:00 +0200

    Integration tests separated module

---
+++ src/integrationTest/java/pl/sg/accountant/AccountantApplicationTests.java
@@ -0,0 +1,18 @@
+package pl.sg.accountant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations="classpath:application-it.properties")
+public class AccountantApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
