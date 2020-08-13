1e2b3dc5d67bae602d5fea98ec1445bab187b857
slawek <slawek.grz@gmail.com>
2020-08-13 21:42:14 +0200

    Login configured

--- src/main/java/pl/sg/application/security/AuthorizationService.java
+++ src/main/java/pl/sg/application/security/AuthorizationService.java
@@ -19,7 +19,7 @@ import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 @Component
 public class AuthorizationService {
     private static final String ROLES = "roles";
-    private static final Duration DURATION = Duration.ofSeconds(10);
+    private static final Duration DURATION = Duration.ofMinutes(10);
 
     private final ApplicationUserRepository applicationUserRepository;
     private final Configuration configuration;
