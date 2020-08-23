99327453e748fc05a3e0b739d63bba5b91d07373
slawek <slawek.grz@gmail.com>
2020-08-23 21:15:43 +0200

    Change data initializator

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,26 +13,26 @@ VALUES (1, 'USER'),
        (2, 'ADMIN'),
        (3, 'USER');
 INSERT INTO account(id, last_transaction_included_in_balance_id, currency, current_balance, name, application_user_id)
-VALUES (4, null, 'PLN', 100.0, 'Konto a', 1),
-       (5, null, 'PLN', 4560, 'Konto b', 1),
-       (6, null, 'EUR', 3000, 'Konto c', 1),
-       (7, null, 'EUR', 20, 'Konto d', 1),
-       (8, null, 'USD', 80, 'Konto e', 1),
-       (9, null, 'USD', 10, 'Konto f', 1),
+VALUES (4, null, 'PLN', 0.0, 'Konto a', 1),
+       (5, null, 'PLN', 0.0, 'Konto b', 1),
+       (6, null, 'EUR', 0.0, 'Konto c', 1),
+       (7, null, 'EUR', 0.0, 'Konto d', 1),
+       (8, null, 'USD', 0.0, 'Konto e', 1),
+       (9, null, 'USD', 0.0, 'Konto f', 1),
 
-       (10, null, 'PLN', 237.7, 'Konto g', 2),
-       (11, null, 'PLN', 98, 'Konto h', 2),
-       (12, null, 'EUR', 4376, 'Konto i', 2),
-       (13, null, 'EUR', 94.9, 'Konto j', 2),
-       (14, null, 'USD', 2354, 'Konto k', 2),
-       (15, null, 'USD', 8787.98, 'Konto l', 2),
+       (10, null, 'PLN', 0.0, 'Konto g', 2),
+       (11, null, 'PLN', 0.0, 'Konto h', 2),
+       (12, null, 'EUR', 0.0, 'Konto i', 2),
+       (13, null, 'EUR', 0.0, 'Konto j', 2),
+       (14, null, 'USD', 0.0, 'Konto k', 2),
+       (15, null, 'USD', 0.0, 'Konto l', 2),
 
-       (16, null, 'PLN', 312.43, 'Konto m', 3),
-       (17, null, 'PLN', 87, 'Konto n', 3),
-       (18, null, 'EUR', 432.4, 'Konto o', 3),
-       (19, null, 'EUR', 87, 'Konto p', 3),
-       (20, null, 'USD', 9874.34, 'Konto r', 3),
-       (21, null, 'USD', 34.45, 'Konto s', 3);
+       (16, null, 'PLN', 0.0, 'Konto m', 3),
+       (17, null, 'PLN', 0.0, 'Konto n', 3),
+       (18, null, 'EUR', 0.0, 'Konto o', 3),
+       (19, null, 'EUR', 0.0, 'Konto p', 3),
+       (20, null, 'USD', 0.0, 'Konto r', 3),
+       (21, null, 'USD', 0.0, 'Konto s', 3);
 
 
 SELECT nextval('hibernate_sequence');
@@ -54,15 +54,15 @@ SELECT nextval('hibernate_sequence');
 SELECT nextval('hibernate_sequence');
 SELECT nextval('hibernate_sequence');
 
-INSERT INTO financial_transaction (id, credit, debit, description, time_of_transaction, application_user_id,
-                                   destination_id, source_id)
-VALUES (22, 0.00, 0.00, 'a', now() + (1 ||' minutes')::interval, 2, 4, 20),
-       (23, 0.00, 0.00, 'b', now() + (2 ||' minutes')::interval, 1, 11, 20),
-       (24, 0.00, 0.00, 'c', now() + (3 ||' minutes')::interval, 3, 4, 11),
-       (25, 0.00, 0.00, 'd', now() + (4 ||' minutes')::interval, 2, 11, 20),
-       (26, 0.00, 0.00, 'e', now() + (5 ||' minutes')::interval, 2, 4, 11),
-       (27, 0.00, 0.00, 'f', now() + (6 ||' minutes')::interval, 2, 11, 12),
-       (28, 0.00, 0.00, 'g', now() + (7 ||' minutes')::interval, 1, 4, 5);
+-- INSERT INTO financial_transaction (id, credit, debit, description, time_of_transaction, application_user_id,
+--                                    destination_id, source_id)
+-- VALUES (22, 0.00, 0.00, 'a', now() + (1 ||' minutes')::interval, 2, 4, 20),
+--        (23, 0.00, 0.00, 'b', now() + (2 ||' minutes')::interval, 1, 11, 20),
+--        (24, 0.00, 0.00, 'c', now() + (3 ||' minutes')::interval, 3, 4, 11),
+--        (25, 0.00, 0.00, 'd', now() + (4 ||' minutes')::interval, 2, 11, 20),
+--        (26, 0.00, 0.00, 'e', now() + (5 ||' minutes')::interval, 2, 4, 11),
+--        (27, 0.00, 0.00, 'f', now() + (6 ||' minutes')::interval, 2, 11, 12),
+--        (28, 0.00, 0.00, 'g', now() + (7 ||' minutes')::interval, 1, 4, 5);
 
 SELECT nextval('hibernate_sequence');
 SELECT nextval('hibernate_sequence');
