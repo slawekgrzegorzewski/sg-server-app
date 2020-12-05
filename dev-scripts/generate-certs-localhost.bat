50773df53828ea228648b4c5ba4e7d69987433e9
slag <slawek.grz@gmail.com>
2020-12-05 12:15:51 +0100

    Corrections

---
+++ dev-scripts/generate-certs-localhost.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Java\jdk-15.0.1\bin\keytool.exe" -genkeypair -alias sg-app -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore keystore.p12 -validity 3650 -storepass slawek
+"C:\Program Files\Java\jdk-15.0.1\bin\keytool.exe" -list -v -storetype pkcs12 -keystore keystore.p12
