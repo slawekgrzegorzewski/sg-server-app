631706cb9cf12361de79d45e2dcf5fbcc21058b4
slag <slawek.grz@gmail.com>
2021-07-15 22:11:41 +0200

    add new script for clearing DB

---
+++ dev-scripts/local-database-recreate.bat
@@ -0,0 +1,3 @@
+set PGPASSWORD=SLAwek1!
+"C:\Program Files\PostgreSQL\13\bin\psql.exe" -h localhost -p 5432 -w -U postgres -c "DROP DATABASE accountant;" postgres
+"C:\Program Files\PostgreSQL\13\bin\psql.exe" -h localhost -p 5432 -w -U postgres -c "CREATE DATABASE accountant;" postgres
