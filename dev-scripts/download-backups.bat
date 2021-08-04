77bf2933617892fad1f5597ce9622abf7d09457e
slag <slawek.grz@gmail.com>
2021-08-04 19:25:06 +0200

    Create download-backups.bat script

---
+++ dev-scripts/download-backups.bat
@@ -0,0 +1,2 @@
+scp slag@grzegorzewski.org:/home/slag/sg-application/database-backups/* "D:\Zrzut produkcji\database-backups"
+ssh slag@grzegorzewski.org rm -rf /home/slag/sg-application/database-backups/*
