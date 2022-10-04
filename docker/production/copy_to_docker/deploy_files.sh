a082431d87d4f8e5e32c0626fb23407febc15db1
slawek <slawek.grz@gmail.com>
2022-10-04 19:41:39 +0200

    refresh management files

---
+++ docker/production/copy_to_docker/deploy_files.sh
@@ -0,0 +1,27 @@
+echo "$1 - postgres password"
+
+echo "localhost:5432:accountant:postgres:$1" > ~/.pgpass
+echo "localhost:5432:postgres:postgres:$1" >> ~/.pgpass
+chmod 400  ~/.pgpass
+
+dos2unix *
+
+mkdir deploy
+mkdir management
+
+cp application.yml.tmpl deploy
+chmod 644 deploy/application.yml.tmpl
+cp docker-compose* deploy
+chmod 644 deploy/docker-compose*
+cp start*.sh deploy
+cp stop.sh deploy
+
+chmod 755 deploy/*.sh
+
+cp backup_data.sh management/
+cp restore_data.sh management/
+cp setup_machine.sh management/
+chmod 755 management/*.sh
+
+cd ../
+rm -rf tmp
