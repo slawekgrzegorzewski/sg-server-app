a8f4cb956583b485b0df58f567f07dd26c17bd5b
slawek <slawek.grz@gmail.com>
2022-10-04 20:43:08 +0200

    refresh management files

--- docker/production/copy_to_docker/start.sh
+++ docker/production/copy_to_docker/start.sh
@@ -2,4 +2,4 @@
 
 ./setup_secrets.sh
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 215372400964.dkr.ecr.eu-central-1.amazonaws.com
-docker stack deploy --with-registry-auth -c docker-compose.yml app
+APPLICATION_YML_VERSION=`date +"%Y-%m-%d_%H-%M-%S"` docker stack deploy --with-registry-auth -c docker-compose.yml app
