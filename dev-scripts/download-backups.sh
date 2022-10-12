d3f075d15b632484c624e80c4e5e8622ae9ce347
slawekgrzegorzewski <slawek.grzegorzewski@satago.com>
2022-10-12 20:03:21 +0200

    download-backups.sh

--- dev-scripts/download-backups.sh
+++ dev-scripts/download-backups.sh
@@ -3,6 +3,6 @@ if [ "$(uname)" = "Linux" ]; then
   scp -i /mnt/d/Development/ovh_private_key slawek@grzegorzewski.org:/home/slawek/management/database-backups/* "/mnt/d/Development/Zrzut produkcji/database-backups"
   ssh -i /mnt/d/Development/ovh_private_key slawek@grzegorzewski.org rm -rf /home/slawek/management/database-backups/*
 else
-  scp -i /mnt/:wq/Development/ovh_private_key slawek@grzegorzewski.org:/home/slawek/management/database-backups/* "D:\Development\Zrzut produkcji\database-backups"
-  ssh -i /mnt/D/Development/ovh_private_key slawek@grzegorzewski.org rm -rf /home/slawek/management/database-backups/*
+  scp -i ~/Development/Slawek/ovh_private_key slawek@grzegorzewski.org:/home/slawek/management/database-backups/* ~/Development/Slawek/backups
+  ssh -i ~/Development/Slawek/ovh_private_key slawek@grzegorzewski.org ls /home/slawek/management/database-backups/*
 fi
