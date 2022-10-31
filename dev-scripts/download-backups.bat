72ab851d81379c548a29ac4fd90b0e8777fbd514
slawek <slawek.grz@gmail.com>
2022-10-31 20:57:45 +0100

    New dev ops scripts

--- dev-scripts/download-backups.bat
+++
@@ -1,2 +0,0 @@
-scp slawek@grzegorzewski.org:/home/slawek/management/database-backups/* "D:\Development\Zrzut produkcji\database-backups"
-ssh slawek@grzegorzewski.org rm -rf /home/slawek/management/database-backups/*
