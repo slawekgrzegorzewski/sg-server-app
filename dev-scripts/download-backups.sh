07e8812d5c5fa1400b00b9eb2626b2734168d6fb
slawekgrzegorzewski <slawek.grzegorzewski@satago.com>
2022-12-04 17:59:46 +0100

    mac os

--- dev-scripts/download-backups.sh
+++ dev-scripts/download-backups.sh
@@ -8,7 +8,7 @@ if [ "$(uname)" = "Linux" ]; then
 else
   scp -i ~/Development/Slawek/ovh_private_key slawek@grzegorzewski.org:/home/slawek/management/database-backups/* ~/Development/Slawek/backups
   ssh -i ~/Development/Slawek/ovh_private_key slawek@grzegorzewski.org ls /home/slawek/management/database-backups/*
-  aws s3 sync s3://intellectualpropertytask ~/Development/Slawek/S3
+  aws s3 sync s3://intellectualpropertytask ~/Development/Slawek/S3 --profile sg-app
   rm -rf ~/Development/Slawek/S3cpy
   cp -r ~/Development/Slawek/S3 ~/Development/Slawek/S3cpy
 fi
