3d3bdd365f9c2ec54bf85eec4a58a8e2810b646c
slawek <slawek.grz@gmail.com>
2022-12-08 22:56:06 +0100

    Migration to Spring 3.0

--- src/main/resources/db/migration/V4__checker_task_interval_to_numeric.sql
+++ src/main/resources/db/migration/V4__checker_task_interval_to_numeric.sql
@@ -1,2 +1,3 @@
-alter table checker_task
-alter column interval type numeric(21, 0)
+alter table checker_task alter column interval type numeric(21, 0);
+
+alter table cube_record alter column time type numeric(21, 0);
