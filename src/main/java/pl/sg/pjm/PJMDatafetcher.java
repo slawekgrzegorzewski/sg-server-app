376bb6797a84f35d9d19b8928093b71dcb5e8a9c
slawekgrzegorzewski <slawek.grzegorzewski@satago.com>
2024-04-04 13:06:07 +0200

    Trigger PJM check in graphql

--- src/main/java/pl/sg/pjm/PJMDatafetcher.java
+++ src/main/java/pl/sg/pjm/PJMDatafetcher.java
@@ -2,6 +2,8 @@ package pl.sg.pjm;
 
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsMutation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import pl.sg.application.security.annotations.TokenBearerAuth;
 import pl.sg.pjm.service.TranslatedBiblesFetcher;
@@ -12,6 +14,7 @@ import java.time.temporal.ChronoUnit;
 
 @DgsComponent
 public class PJMDatafetcher {
+    private static final Logger LOG = LoggerFactory.getLogger(PJMDatafetcher.class);
     private final ScheduledTaskRegistrar scheduledTaskRegistrar;
     private final TranslatedBiblesFetcher translatedBiblesFetcher;
 
@@ -23,14 +26,18 @@ public class PJMDatafetcher {
     @DgsMutation
     @TokenBearerAuth(any = "PJM")
     public String triggerTranslatedPJMVersesCheck() {
+        LOG.info("Scheduling fetching translated PJM verses");
         scheduledTaskRegistrar.addOneTimeTask(() -> {
             try {
+                LOG.info("Starting fetching translated PJM verses");
                 translatedBiblesFetcher.fetchTranslatedVerses();
+                LOG.info("Finished fetching translated PJM verses");
             } catch (IOException e) {
+                LOG.info("Error during fetching translated PJM verses");
                 throw new RuntimeException(e);
             }
         }, Duration.of(1, ChronoUnit.SECONDS));
-
+        LOG.info("Scheduled fetching translated PJM verses");
         return "OK";
     }
 }
