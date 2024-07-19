c8fe0f7b0a9d09aa99dba0d6922a6cb19b24c056
slawek <slawek.grz@gmail.com>
2024-07-19 22:23:14 +0200

    move to three node docker swarm

--- docker/setup/deploy_files.sh
+++ docker/setup/deploy_files.sh
@@ -1,16 +1,21 @@
 source ./setup_directories.sh
 
-sudo chmod 400  ~/.pgpass
-
+CURRENT_DIR=$(pwd)
 SOURCE_CONFIG_DIR=../config
 SOURCE_STACK_DIR=..
 SOURCE_MANAGEMENT_DIR=../management
 
-sudo dos2unix $SOURCE_CONFIG_DIR/*
-sudo dos2unix $SOURCE_STACK_DIR/*
-sudo dos2unix $SOURCE_MANAGEMENT_DIR/*
-sudo chmod 644 $SOURCE_STACK_DIR/*
-sudo cp -f $SOURCE_MANAGEMENT_DIR/* $MANAGEMENT_DIR
+cd $SOURCE_CONFIG_DIR
+sudo dos2unix application.yml.tmpl  logback.xml  nginx.conf
+
+cd $SOURCE_STACK_DIR
+sudo dos2unix docker-compose.yml
+sudo chmod 644 docker-compose.yml
+
+cd $SOURCE_MANAGEMENT_DIR
+sudo dos2unix *.sh
+
+sudo cp -f $SOURCE_MANAGEMENT_DIR/*.sh $MANAGEMENT_DIR
 sudo cp -f $SOURCE_STACK_DIR/docker-compose.yml $STACK_DIR
 sudo cp -f $SOURCE_CONFIG_DIR/logback.xml $CONFIG_DIR
 sudo cp -f $SOURCE_CONFIG_DIR/nginx.conf $CONFIG_DIR
