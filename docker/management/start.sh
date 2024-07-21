816cfb49696358e65e97f0e0647596d79ead7456
slawek <slawek.grz@gmail.com>
2024-07-21 08:26:59 +0200

    move to three node docker swarm

--- docker/management/start.sh
+++ docker/management/start.sh
@@ -5,7 +5,7 @@ STACK_DIR=$APPLICATION_DIR/stack
 SECRETS_DIR=$APPLICATION_DIR/secrets
 
 source $APPLICATION_DIR/secrets/put_secrets_to_env.sh
-$SECRETS_DIR/setup_secrets.sh $STACK_DIR/registry/auth
+$SECRETS_DIR/setup_secrets.sh $STACK_DIR/config
 docker login -p $REGISTRY_PASSWORD -u $REGISTRY_USER $REGISTRY_URL
 
 cd $STACK_DIR || exit
