f27feef9e31653b4106934aa7a616ceda6b11a5b
slawek <slawek.grz@gmail.com>
2024-09-05 20:32:20 +0200

    update backup app

--- dev-ops/tools/backup-app/download-backups.sh
+++ dev-ops/tools/backup-app/download-backups.sh
@@ -2,8 +2,8 @@
 
 DB_DIR=/development/DB
 
-sshpass -f /development/ssh_pass scp -P 24 slawek@grzegorzewski.org:/home/slawek/Cluster/permanent_data/database/backups/* "$DB_DIR"
-sshpass -f /development/ssh_pass ssh -p 24 slawek@grzegorzewski.org rm -rf /home/slawek/Cluster/permanent_data/database/backups/*
+sshpass -f /development/ssh_pass scp -P 101 slawek@grzegorzewski.org:/home/slawek/Cluster/permanent_data/database/backups/* "$DB_DIR"
+sshpass -f /development/ssh_pass ssh -p 101 slawek@grzegorzewski.org rm -rf /home/slawek/Cluster/permanent_data/database/backups/*
 aws s3 sync --delete s3://intellectualpropertytask /development/S3
 rm -rf /development/S3cpy
 cp -r /development/S3 /development/S3cpy
